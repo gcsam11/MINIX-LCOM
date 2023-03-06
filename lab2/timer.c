a4132e18c8243ddb4eac115eb6f2851605b78175
David Cordeiro <up202108820@up.pt>
2023-03-06 10:52:33 +0000

    finished timer_test_read_config()

--- lab2/timer.c
+++ lab2/timer.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include "i8254.h"
+#include "utils.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   /* To be implemented by the students */
@@ -32,16 +33,58 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t rb_cmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
 
-  return 1;
+  if (sys_outb(TIMER_CTRL, rb_cmd) != F_OK) {
+    printf("AN ERROR OCURRED WHEN WRITTING THE READ_BACK COMMAND TO THE TIMER CONTROL PORT\n");
+    return 1;
+  }
+
+  int port = 0;
+
+  if (timer == 0) {
+    port = TIMER_0;
+  } else if (timer == 1) {
+    port = TIMER_1;
+  } else if (timer == 2) {
+    port = TIMER_2;
+  } else {
+    printf("INVALID TIMER VALUE");
+  }
+  
+  if (utils_sys_inb(port, st) != F_OK) {
+    printf("AN ERROR OCURRED WHEN READING THE STATUS BYTE FROM THE DESIRED TIMER\n");
+    return 1;
+  }
+  
+  return 0;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
+  union timer_status_field_val config;
 
-  return 1;
+  if (field == tsf_all) {
+    config.byte = st;
+  } else if (field == tsf_initial) {
+    config.in_mode = (st & TIMER_LSB_MSB) >> 4;
+  } else if (field == tsf_mode) {
+    config.count_mode = (st & (BIT(1) | BIT(2) | BIT(3)) >> 1);
+
+    //make values 110 and 111, equal to 2 and 3 respectively
+    if (config.count_mode > 5) {
+      config.count_mode = config.count_mode ^ BIT(2);
+    }
+  } else if (field == tsf_base) {
+    config.bcd = st & TIMER_BCD;
+  } else {
+    printf("THE STATUS FIELD RECEIVED IS INCORRECT\n");
+    return 1;
+  }
+
+  if (timer_print_config(timer, field, config) != F_OK) {
+    printf ("AN ERROR OCURRED WHEN TRYING TO PRINT THE CONFIGURATION\n");
+    return 1;
+  }
+
+  return 0;
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -17,8 +17,14 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint32_t val = 0;
 
-  return 1;
+  if (sys_inb(port,  &val) != F_OK) {
+    printf("AN ERROR OCURRED WHEN READING FROM THE PORT USING sys_inb\n");
+    return 1;
+  }
+  
+  *value = (uint8_t)val;
+
+  return 0;
 }
