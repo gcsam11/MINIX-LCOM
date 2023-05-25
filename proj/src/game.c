4d1b4fb3b23a4d082b5e4e649b2504c69c074c93
Gonçalo Martins <goncalo.csam@gmail.com>
2023-05-25 18:33:36 +0100

    Fixed XPM's

--- proj/src/game.c
+++ proj/src/game.c
@@ -19,8 +19,8 @@ Sprite* planthero;
 Sprite* mouse;
 Sprite* zombie;
 Sprite* play_button;
-//Sprite* date_button;
-//Sprite* quit_button;
+Sprite* date_button;
+Sprite* quit_button;
 
 void (render_frame)() {
     vg_clear_frame();
@@ -43,12 +43,12 @@ void (set_game_state)(enum game_state_t state) {
     switch (state) {
         case MENU: {
             vg_set_background(backgroundMainMenu_xpm);
-            set_sprite_x(mouse, 512);
-            set_sprite_y(mouse, 384);
 
             memset(render_sprites, 0, sizeof(render_sprites));
-            render_sprites[0] = mouse;
-            render_sprites[1] = play_button;
+            render_sprites[0] = play_button;
+            render_sprites[1] = date_button;
+            render_sprites[2] = quit_button;
+            render_sprites[3] = mouse;
 
             render_frame();
             
@@ -88,9 +88,9 @@ void (game_init)() {
     planthero = create_sprite(planthero_xpm, 0, 0, 0, 0);
     zombie = create_sprite(zombie_xpm, 400, 350, 0, 0);
     mouse = create_sprite(mouse_xpm, 518, 384, 0, 0);
-    play_button = create_sprite(play_white_xpm, 450, 150, 0, 0);
-    //date_button = create_sprite(date_white_xpm, 450, 250, 0, 0);
-    //quit_button = create_sprite(quit_white_xpm, 450, 350, 0, 0);
+    play_button = create_sprite(play_white_xpm, 300, 100, 0, 0);
+    date_button = create_sprite(date_white_xpm, 300, 200, 0, 0);
+    quit_button = create_sprite(quit_white_xpm, 300, 300, 0, 0);
 
     set_game_state(MENU);
 }
@@ -141,8 +141,8 @@ void (game_exit)() {
     destroy_sprite(&zombie);
     destroy_sprite(&mouse);
     destroy_sprite(&play_button);
-    //destroy_sprite(&date_button);
-    //destroy_sprite(&quit_button);
+    destroy_sprite(&date_button);
+    destroy_sprite(&quit_button);
 
     vg_exit();
 }
